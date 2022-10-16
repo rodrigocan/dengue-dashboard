bb4797f0d57daf8b72f880820c935300765b8575
Rodrigo Cândido <rodrigocan@gmail.com>
2022-10-16 16:15:48 -0300

    Série temporal de casos por ano, por estado

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import numpy as np
 @st.cache
 def load_data():
   data = pd.read_csv('dengue_geral.csv')
+  data[['cod_mun', 'cod_est']] = data[['cod_mun', 'cod_est']].astype(str)
+  data['ano'] = pd.to_datetime(data['ano'], format='%Y').dt.year
+  data['num_casos'] = data['num_casos'].astype(int)
   return data
 
 data = load_data()
@@ -22,9 +25,10 @@ estados = np.sort(data.estado.unique())
 estado = st.selectbox('Selecione um estado', estados)
 
 data_estado = data[data['estado'] == estado]
-total_casos = data_estado['num_casos'].sum()
+total_casos_ano = data_estado.groupby(['ano']).num_casos.sum().reset_index()
 
-st.write('Você selecionou:', estado,'. Total de casos notificados:', total_casos)
-data_estado
+st.write('Você selecionou:', estado)
+st.line_chart(total_casos_ano, x = 'ano', y = 'num_casos')
+total_casos_ano
 
 st.subheader('Por município')
